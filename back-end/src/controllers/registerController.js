d9b9ef0a586d62cad6783834fd446b24eae33233
ClawNarok <arthurvgf@gmail.com>
2022-09-23 19:01:50 -0300

    Validando email e nome iguais no registro

--- back-end/src/controllers/registerController.js
+++ back-end/src/controllers/registerController.js
@@ -4,7 +4,7 @@ const registerService = require('../services/registerService');
 const registerController = {
   async register(req, res) {
     const data = await registerService.validateBodyRegister(req.body);
-    await registerService.getByEmailOrThrows(data);
+    await registerService.getByUserOrThrows(data);
     const user = await registerService.createUser(data);
     const { password, id, ...dataUser } = user;
     const token = await loginService.makeToken({ user });
--- back-end/src/services/registerService.js
+++ back-end/src/services/registerService.js
@@ -4,34 +4,39 @@ const { throwConflictError } = require('./utils');
 const { Users } = require('../database/models');
 
 const registerService = {
-    async validateBodyRegister(body) {
-      const schema = Joi.object({
-        name: Joi.string().required().min(12),
-        email: Joi.string().required().email().max(255),
-        password: Joi.string().required().min(6),
+  async validateBodyRegister(body) {
+    const schema = Joi.object({
+      name: Joi.string().required().min(12),
+      email: Joi.string().required().email().max(255),
+      password: Joi.string().required().min(6),
+    });
+    const data = await schema.validateAsync(body);
+    return data;
+  },
+  async getByUserOrThrows(data) {
+      const { email, name } = data;
+      const userEmail = await Users.findOne({
+        where: { email },
+        raw: true,
       });
-      const data = await schema.validateAsync(body);
-      return data;
-    },
-    async getByEmailOrThrows(data) {
-        const { email } = data;
-        const user = await Users.findOne({
-          where: { email },
-          raw: true,
-        });
-        if (user) throwConflictError('Email já cadastrado');
-    },
-    async createUser({ name, password, email }) {
-      console.log(name);
-        const payload = {
-         name,
-         password: md5(password),
-         email,
-         role: 'customer',
-        };
-        const { dataValues } = await Users.create(payload, { raw: true });
-        return dataValues;
-    },
+      if (userEmail) throwConflictError('Email já cadastrado');
+      const userName = await Users.findOne({
+        where: { name },
+        raw: true,
+      });
+      if (userName) throwConflictError('Nome já cadastrado');
+  },
+  async createUser({ name, password, email }) {
+    console.log(name);
+      const payload = {
+        name,
+        password: md5(password),
+        email,
+        role: 'customer',
+      };
+      const { dataValues } = await Users.create(payload, { raw: true });
+      return dataValues;
+  },
 };
 
 module.exports = registerService;
