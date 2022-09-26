9a53432248f0ea77341f3b9c03a4ab7d53e32ab1
ClawNarok <arthurvgf@gmail.com>
2022-09-26 16:35:29 -0300

    removendo arquivo com nome errado

--- back-end/src/routes/RegisterRoutes.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require('express');
-const registerController = require('../controllers/registerController');
-
-const registerRoute = Router();
-
-registerRoute.post('/', registerController.register);
-
-module.exports = registerRoute;
